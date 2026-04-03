ca5e21063dabde2bbabcaf311c1d17c4a0707261
mansi <mansiakkr@gmail.com>
2026-04-03 17:32:50 +0530

    Added OrderItem - entity, DTO, repository

---
+++ src/main/java/com/springboot/flyway/dbmigration/dto/request/OrderItemDto.java
@@ -0,0 +1,12 @@
+package com.springboot.flyway.dbmigration.dto.request;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class OrderItemDto {
+    private Long productId;
+    private Integer quantity;
+    private BigDecimal unitPrice;
+}
