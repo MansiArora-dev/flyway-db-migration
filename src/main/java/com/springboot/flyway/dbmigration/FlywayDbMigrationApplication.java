465fb52eea496513e8607793fcab7957ebb48223
mansi <mansiakkr@gmail.com>
2026-04-03 14:06:43 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/springboot/flyway/dbmigration/FlywayDbMigrationApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.flyway.dbmigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayDbMigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayDbMigrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/flyway/dbmigration/FlywayDbMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.flyway.dbmigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayDbMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
