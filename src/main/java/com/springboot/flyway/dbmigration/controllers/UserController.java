aeeb1366d3772bb0f7745d5d8fbf07ca11f2804e
mansi <mansiakkr@gmail.com>
2026-04-03 17:26:56 +0530

    Added User - entity, DTO, repository, service and controller

--- src/main/java/com/springboot/flyway/dbmigration/controllers/UserController.java
+++ src/main/java/com/springboot/flyway/dbmigration/controllers/UserController.java
@@ -40,4 +40,5 @@ public class UserController {
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
     }
+
 }
--- src/main/java/com/springboot/flyway/dbmigration/entities/User.java
+++ src/main/java/com/springboot/flyway/dbmigration/entities/User.java
@@ -23,4 +23,5 @@ public class User {
 
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt = LocalDateTime.now();
+
 }
