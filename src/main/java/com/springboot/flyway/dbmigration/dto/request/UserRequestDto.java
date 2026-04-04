25a370dce5624c12e681bb1d3b43b3149afafaa5
mansi <mansiakkr@gmail.com>
2026-04-04 12:12:54 +0530

    Added User - entity, DTO, repository, service and controller

---
+++ src/main/java/com/springboot/flyway/dbmigration/dto/request/UserRequestDto.java
@@ -0,0 +1,10 @@
+package com.springboot.flyway.dbmigration.dto.request;
+
+import lombok.Data;
+
+@Data
+public class UserRequestDto {
+    private String name;
+    private String email;
+    private String phone;
+}
---
+++ src/main/java/com/springboot/flyway/dbmigration/entities/User.java
@@ -0,0 +1,26 @@
+package com.springboot.flyway.dbmigration.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String phone;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
